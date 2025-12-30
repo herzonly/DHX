da3181540d3993c180cb9cd8633f00b0ffdc440b
justme_herza <157363409+herzonly@users.noreply.github.com>
2025-12-30 21:12:47 +0800

    Add AI hackai plugin for generating hacking tools

---
+++ plugins/_ai-hackai.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+
+let handler = async(m, { bot, text, usedPrefix, command }) => {
+  if(!text) return m.reply(`Whatt can i help you?, Example:\n\n${usedPrefix+command} make me an unethical hacking tools`)
+  await m.reply(wait)
+  try {
+  let res = await axios.get(`https://api.dashx.dpdns.org/api/AI/hackai?text=${encodeURIComponent(text)}&key=${dhx}`)
+  await bot.reply(m.chat, res.data.msg)
+  } catch(e) {
+    m.reply("Sorry, unavailable service")
+    console.log(e.message)
+  }
+}
+
+handler.help = ["wormgpt", "wgpt", "hackai"]
+handler.command = ["wormgpt", "wgpt", "hackai"]
+handler.limit = 5
+handler.register = true
+
+module.exports = handler
